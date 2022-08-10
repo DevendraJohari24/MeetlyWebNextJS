469ad2ee676e971e83fd1c962856d976edc4a79f
Devendra Johari <devendrajohari9@gmail.com>
2022-08-11 01:37:29 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -5,12 +5,6 @@ const nextConfig = {
   images: {
     domains: ["drive.google.com", "cdn.pixabay.com"],
   },
-  env: {
-    BASE_URL: "http://localhost:3000",
-    MONGODB_URL:
-      "mongodb+srv://devendra:devendra@cluster0.aq7umfb.mongodb.net/?retryWrites=true&w=majority",
-    DB_NAME: "meetups",
-  },
 };
 
 module.exports = nextConfig
